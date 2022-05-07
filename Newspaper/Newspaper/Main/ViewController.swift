73d2210b0e276139bac21d6a3472de6c964c8710
lonatelled <97543628+lonatelled@users.noreply.github.com>
2022-05-07 19:13:20 +0300

    Delete Pods

--- Newspaper/Newspaper/Main/ViewController.swift
+++ Newspaper/Newspaper/Main/ViewController.swift
@@ -9,13 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var enterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let storyboard = UIStoryboard(name: "Tabbar", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "Tabbar")
-        self.navigationController?.pushViewController(vc, animated: false)
+//        let storyboard = UIStoryboard(name: "Tabbar", bundle: nil)
+//        let vc = storyboard.instantiateViewController(withIdentifier: "Tabbar")
+//        self.navigationController?.pushViewController(vc, animated: false)
     }
 
-
+    @IBAction func switchLogin(_ sender: UIButton) {
+    }
+    
 }
 
---
+++ Newspaper/Newspaper/Tabbar/feed/FeedTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedTableViewCell.swift
+//  Newspaper
+//
+//  Created by Дарья Каркоцкая on 7.05.22.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var postName: UILabel!
+    @IBOutlet weak var postDate: UILabel!
+    @IBOutlet weak var postText: UILabel!
+    @IBOutlet weak var postImage: UIImageView!
+    
+
+}
